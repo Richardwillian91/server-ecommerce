17341ef9fb2877294fdc883cf69a52efcc9cc806
Richard Silva <richardwillian166@hotmail.com>
2021-05-13 13:38:50 -0300

    Lista os usuários

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require ('express')
 const mongoose = require('mongoose')
-const cors = require('cors')
+const cors = require('cors')// Importa as dependências
 
-const routes = require ('./routes')
+const routes = require ('./routes')// importa as rotas
 
 require('dotenv').config()
 const app = express()
@@ -10,12 +10,12 @@ const app = express()
 mongoose.connect(process.env.MONGO_URI,{
     useUnifiedTopology: true,
     useNewUrlParser: true
-}, console.log('Connected to database'))
+}, console.log('Connected to database')) //Conexão com o banco
 
 
 app.use(cors())
 app.use(express.json())
-app.use(routes)
+app.use(routes) // usa as dependencias
 
 
-app.listen(3000, () => console.log('Server is running'))
+app.listen(3000, () => console.log('Server is running')) // porta e retorna se está funcionando o servidor
